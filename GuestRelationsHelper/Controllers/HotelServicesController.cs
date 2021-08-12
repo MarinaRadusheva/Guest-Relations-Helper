55aea90f984df8160f0e47cee8189e64338b6a4b
Marina <marinaradusheva9@gmail.com>
2021-08-12 13:35:15 +0300

    added hotelServices controller

---
+++ GuestRelationsHelper/Controllers/HotelServicesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static GuestRelationsHelper.WebConstants;
+
+namespace GuestRelationsHelper.Controllers
+{
+    [Authorize(Roles = GuestRoleName)]
+    public class HotelServicesController : Controller
+    {
+        public IActionResult Dining()
+        {
+            return View();
+        }
+        public IActionResult Transport()
+        {
+            return View();
+        }
+        public IActionResult Cleaning()
+        {
+            return View();
+        }
+    }
+}
